9a2a24221e7d354ecc61055e43f03e504ddc82b7
MochammadTaufikJuliandi <taufik.juliandi4@gmail.com>
2022-12-04 21:47:46 +0700

    ganti posisi artikel

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,31 +1,31 @@
-import React from 'react'
+import React from "react";
 import { useState, useEffect } from "react";
-import { db } from '../firebase-config';
-import {collection, getDocs} from 'firebase/firestore'
-
-
+import { db } from "../firebase-config";
+import { collection, getDocs } from "firebase/firestore";
 
 export default function ArticleList() {
-    const [article, setArticle] = useState([])
-    useEffect(()=>{
-        const getData = async ()=>{
-            const articlesCollectionRef = collection(db, "article")
-            const data = await getDocs(articlesCollectionRef)
-            setArticle(data.docs.map(doc => ({...doc.data(), id:doc.id})))
-        }
-        getData()
-    },[])
+  const [article, setArticle] = useState([]);
+  useEffect(() => {
+    const getData = async () => {
+      const articlesCollectionRef = collection(db, "article");
+      const data = await getDocs(articlesCollectionRef);
+      setArticle(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getData();
+  }, []);
 
   return (
     <div>
-        {article.map((articles) =>{
-            return (<div key={articles.id}> 
-                <h1>{articles.title}</h1>
-                <p>{articles.body}</p>
-                <img src={articles.img} alt='' />
-                <p>Category : {articles.category}</p>
-                </div>)
-        })}
+      {article.map((articles) => {
+        return (
+          <div key={articles.id}>
+            <h1>{articles.title}</h1>
+            <p>Category : {articles.category}</p>
+            <img src={articles.img} alt="" />
+            <p>{articles.body}</p>
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
